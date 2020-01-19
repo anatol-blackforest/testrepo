de6594d3759aef80e692671235f4bbf8e8521a82
anatol-blackforest <anatolblackforest@gmail.com>
2020-01-19 15:32:38 +0200

    chinese site

--- connector.js
+++ connector.js
@@ -6,24 +6,40 @@ const io = require('socket.io')(server);
 const path = require('path');
 
 const ccxws = require("ccxws");
-const binance = new ccxws.binance();
+const binance = new ccxws.binance()
+const zb = new ccxws.zb();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(3000, () => console.log(`It works!`));
 
-const market = {
+const marketBinance = {
     id: "BTCUSDT",
+    base: "BTC",
+    quote: "USDT",
+};
+
+const marketZB = {
+    id: "btc_usdt",
     base: "BTC",
     quote: "USDT",
 };
 
 binance.on("l2snapshot", snapshot => {
     data = {
-        asks: snapshot.asks.slice(0, 3),
-        bids: snapshot.bids.slice(0, 3)
+        asks: snapshot.asks.slice(0, 10),
+        bids: snapshot.bids.slice(0, 10)
     };
-    io.send(data)
+    io.emit("binance", data)
+});
+
+zb.on("l2snapshot", snapshot => {
+    data = {
+        asks: snapshot.asks.slice(0, 10),
+        bids: snapshot.bids.slice(0, 10)
+    };
+    io.emit("zb", data)
 });
    
-binance.subscribeLevel2Snapshots(market);
+binance.subscribeLevel2Snapshots(marketBinance);
+zb.subscribeLevel2Snapshots(marketZB);
--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,8 @@
 jQuery(document).ready(function () {
 
     const socket = io.connect('http://localhost:3000');
-    socket.emit('loaded')
 
-    socket.on('message', function(result){
+    socket.on('binance', function(result){
 
         let asks = "";
         let bids = "";
@@ -11,11 +10,27 @@ jQuery(document).ready(function () {
         result.asks.forEach((item, index, array) => asks += Math.floor(item.price) + " / " + (+item.size).toFixed(1) + "<br />")
         result.bids.forEach((item, index, array) => bids += Math.floor(item.price) + " / " + (+item.size).toFixed(1) + "<br />")
 
-        $('#asks').empty()
-        $('#bids').empty()
+        $('#binance #asks').empty()
+        $('#binance #bids').empty()
 
-        $('#asks').html(asks)
-        $('#bids').html(bids)
+        $('#binance #asks').html(asks)
+        $('#binance #bids').html(bids)
+
+    })
+
+    socket.on('zb', function(result){
+
+        let asks = "";
+        let bids = "";
+
+        result.asks.forEach((item, index, array) => asks += Math.floor(item.price) + " / " + (+item.size).toFixed(1) + "<br />")
+        result.bids.forEach((item, index, array) => bids += Math.floor(item.price) + " / " + (+item.size).toFixed(1) + "<br />")
+
+        $('#zb #asks').empty()
+        $('#zb #bids').empty()
+
+        $('#zb #asks').html(asks)
+        $('#zb #bids').html(bids)
 
     })
 })
